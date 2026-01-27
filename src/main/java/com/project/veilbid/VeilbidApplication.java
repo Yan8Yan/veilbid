48d28e3f597548bf334577cebd2500674b4410e3
Yan <ahha271104@mail.ru>
2026-01-27 23:19:42 +0300

    Initial commit

---
+++ src/main/java/com/project/veilbid/VeilbidApplication.java
@@ -0,0 +1,13 @@
+package com.project.veilbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeilbidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeilbidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/veilbid/VeilbidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.veilbid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeilbidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
