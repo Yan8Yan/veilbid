5217798955345f107f0ed6d26202e7cc727a3d2a
Yan <145013972+Yan8Yan@users.noreply.github.com>
2026-02-06 16:15:04 +0300

    Created UserRepository interface and implemented the UserProvisioningFilter

---
+++ src/main/java/com/project/veilbid/filters/UserProvisioningFilter.java
@@ -0,0 +1,56 @@
+package com.project.veilbid.filters;
+
+import com.project.veilbid.domain.entities.User;
+import com.project.veilbid.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+//выполняется один раз на каждый http запрос
+public class UserProvisioningFilter extends OncePerRequestFilter {
+
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal
+            (HttpServletRequest request,
+             HttpServletResponse response,
+             FilterChain filterChain) throws ServletException, IOException {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication != null
+                && authentication.isAuthenticated()
+                && authentication.getPrincipal() instanceof Jwt jwt)
+        {
+            UUID keycloakId = UUID.fromString(jwt.getSubject());
+
+            if(!userRepository.existsById(keycloakId))
+            {
+                User user = new User();
+                user.setId(keycloakId);
+                user.setName(
+                        jwt.getClaims().get("preferred_username").toString()
+                );
+
+                user.setEmail(jwt.getClaimAsString("email"));
+
+                userRepository.save(user);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
